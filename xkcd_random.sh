5beb543428c034e0e589bcddcd93162747cbae66
John Alessi <john@localhost.localdomain>
2015-12-06 01:43:11 -0500

    first commit

---
+++ xkcd_random.sh
@@ -0,0 +1,8 @@
+URL=$(curl "http://xkcd.com/$((RANDOM%1600))/info.0.json" | grep -o "img.*" | cut -d \" -f 3 | sed -e 's/\\//g')
+BASENAME=$(basename $URL)
+echo $URL
+echo $BASENAME
+
+wget $URL
+feh $BASENAME
+rm $BASENAME
